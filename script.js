1b9669bae705e041aea831daaf2c75b253a4ead0
HARIPRASAD NAKKA <108853643+hari256387@users.noreply.github.com>
2023-07-30 13:49:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function calculateAge() {
+    const birthdate = document.getElementById('birthdate').value;
+    const today = new Date();
+    const birthDate = new Date(birthdate);
+    
+    if (isNaN(birthDate)) {
+        alert("Please enter a valid date.");
+        return;
+    }
+
+    const ageInMilliseconds = today - birthDate;
+    const ageDate = new Date(ageInMilliseconds);
+
+    const years = ageDate.getUTCFullYear() - 1970;
+    const months = ageDate.getUTCMonth();
+    const days = ageDate.getUTCDate() - 1;
+
+    document.getElementById('result').innerText = `You are ${years} years, ${months} months, and ${days} days old.`;
+}
